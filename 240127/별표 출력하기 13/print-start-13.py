220c9886b1c761a7c796c7cde1a1e1ae011c71c2
choidh1203 <98026205+choidh1203@users.noreply.github.com>
2024-01-27 19:29:27 +0900

    Title: 별표 출력하기 13, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240127/별표 출력하기 13/print-start-13.py
+++ 240127/별표 출력하기 13/print-start-13.py
@@ -12,11 +12,22 @@ for i in range(1,n+1):
         b += 1
         print('* '*b)
         
-for i in range(1,n+1):
-    if i %2 == 1:
-        print('* '*a)
-        a += 1
-        
-    else:
-        print('* '*b)
-        b -= 1
+if n%2 == 1:
+    for i in range(1,n+1):
+        if i %2 == 1:
+            print('* '*a)
+            a += 1
+            
+        else:
+            print('* '*b)
+            b -= 1
+
+if n%2 == 0:
+    for i in range(1,n+1):
+        if i %2 == 0:
+            print('* '*a)
+            a += 1
+            
+        else:
+            print('* '*b)
+            b -= 1
