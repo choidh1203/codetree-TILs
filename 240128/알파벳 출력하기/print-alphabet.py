f35a145c298d00ca3403b01f9ea91421601092c3
choidh1203 <98026205+choidh1203@users.noreply.github.com>
2024-01-28 18:26:58 +0900

    Title: 알파벳 출력하기, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240128/알파벳 출력하기/print-alphabet.py
+++ 240128/알파벳 출력하기/print-alphabet.py
@@ -2,8 +2,7 @@ n = int(input())
 cnt = 65
 
 for i in range(n):
-    for j in range(n):
-        if i >= j:
-            print(chr(cnt),end="")
-            cnt += 1
+    for j in range(i+1):
+        print(chr(cnt),end="")
+        cnt += 1
     print()
