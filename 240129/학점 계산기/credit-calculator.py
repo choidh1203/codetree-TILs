057075fa34f20568410ca689d38650c7907978ec
choidh1203 <98026205+choidh1203@users.noreply.github.com>
2024-01-29 20:29:13 +0900

    Title: 학점 계산기, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240129/학점 계산기/credit-calculator.py
+++ 240129/학점 계산기/credit-calculator.py
@@ -1,14 +1,14 @@
 n = int(input())
 
-arr = list(map(int, input().split()))
+arr = list(map(float, input().split()))
 sum = 0
 
 for i in range(n):
     sum += arr[i]
 print("{:.1f}".format(sum/n))
-if sum > 4.0:
+if sum/n > 4.0:
     print('Perfect')
-elif sum > 3.0:
+elif sum/n > 3.0:
     print('Good')
 else:
     print('Poor')
