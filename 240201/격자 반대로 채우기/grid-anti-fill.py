64434b4771c9c0f502499f8199d883a22dcf0a8d
choidh1203 <98026205+choidh1203@users.noreply.github.com>
2024-02-01 22:46:01 +0900

    Title: 격자 반대로 채우기, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 240201/격자 반대로 채우기/grid-anti-fill.py
+++ 240201/격자 반대로 채우기/grid-anti-fill.py
@@ -14,7 +14,7 @@ for i in range(n-1,-1,-1):
                 arr[j][i] = cnt
                 cnt += 1
     else:
-        if i%2 == 1:
+        if i%2 == 0:
             for j in range(n-1,-1,-1):
                 arr[j][i] = cnt
                 cnt += 1
