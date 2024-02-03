3eba8be5e7e05205ef0e24718f7beb2ce0b7a79e
choidh1203 <98026205+choidh1203@users.noreply.github.com>
2024-02-03 19:43:35 +0900

    Title: 특정 문자의 등장 횟수, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

--- 240203/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
+++ 240203/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
@@ -1,3 +1,9 @@
 a = input()
-
-print(a.count('ee'), a.count('eb'))
+cnt1 = 0
+cnt2 = 0
+for i in range(len(a)-1):
+    if 'ee' == a[i:i+2]:
+        cnt1 += 1
+    if 'eb' == a[i:i+2]:
+        cnt2 += 1
+print(cnt1, cnt2)
