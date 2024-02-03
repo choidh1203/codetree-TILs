6d39f08f307dacb1a61aaaabb4a9127771877aad
choidh1203 <98026205+choidh1203@users.noreply.github.com>
2024-02-03 18:24:28 +0900

    Title: Run Length 인코딩, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240203/Run Length 인코딩/run-length-encoding.py
@@ -0,0 +1,16 @@
+string = input()
+
+char = [string[0]]
+idx = [1]
+
+for i in range(1,len(string)):
+    if string[i] == string[i-1]:
+        idx[-1] += 1
+    else:
+        char.append(string[i])
+        idx.append(1)
+
+print(len(char)+len(idx))
+
+for i in range(len(char)):
+    print(char[i]+str(idx[i]),end="")
