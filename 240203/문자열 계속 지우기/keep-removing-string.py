1c1ba71310f3f8858e406b8b88aaa1221d4af4a6
choidh1203 <98026205+choidh1203@users.noreply.github.com>
2024-02-03 22:28:16 +0900

    Title: 문자열 계속 지우기, Time: 155ms, Memory: 24MB, Status: Passed - CodeTree

--- 240203/문자열 계속 지우기/keep-removing-string.py
+++ 240203/문자열 계속 지우기/keep-removing-string.py
@@ -4,7 +4,7 @@ b = list(input())
 exist = True
 
 while exist:
-    for i in range(len(a)-len(b)):
+    for i in range(len(a)-len(b)+1):
         if b == a[i:i+len(b)]:
             del a[i:i+len(b)]
     if not "".join(b) in "".join(a):
