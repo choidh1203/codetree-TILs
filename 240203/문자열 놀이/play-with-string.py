186f58719f5062d40aea921156e3a902a500630c
choidh1203 <98026205+choidh1203@users.noreply.github.com>
2024-02-03 21:08:17 +0900

    Title: 문자열 놀이, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240203/문자열 놀이/play-with-string.py
@@ -0,0 +1,15 @@
+s, q = input().split()
+
+for i in range(int(q)):
+    n, a, b = input().split()
+    n = int(n)
+    if n == 1:
+        a = int(a)
+        b = int(b)
+        new = list(s)
+        new[a-1] = s[b-1]
+        new[b-1] = s[a-1]
+        s = "".join(new)
+        print(s)
+    elif n == 2:
+        print(s.replace(a,b))
