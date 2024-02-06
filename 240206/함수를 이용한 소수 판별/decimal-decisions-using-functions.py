43327ed725ef3d1fe1126321db640e4c3303a4d5
choidh1203 <98026205+choidh1203@users.noreply.github.com>
2024-02-06 14:51:16 +0900

    Title: 함수를 이용한 소수 판별, Time: 148ms, Memory: 24MB, Status: Passed - CodeTree

--- 240206/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 240206/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -7,11 +7,13 @@ def prime_map(n):
 
 def main(start, end):
     prime_sum = 0
+    if end == 1:
+        return 0
     for i in range(start, end+1):
         if prime_map(i):
             prime_sum += i
-    print(prime_sum)
+    return prime_sum
 
 a, b = map(int,input().split())
 
-main(a,b)
+print(main(a,b))
