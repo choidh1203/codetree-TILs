c2f5cad1797347db43afb772313c4dcee18ad608
choidh1203 <98026205+choidh1203@users.noreply.github.com>
2024-02-07 22:44:40 +0900

    Title: 2021년 날짜의 유무, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240207/2021년 날짜의 유무/with-or-without-2021.py
+++ 240207/2021년 날짜의 유무/with-or-without-2021.py
@@ -5,13 +5,13 @@ def date(m,d):
                 print('Yes')
             else:
                 print('No')
-        elif m%2 == 0:
-            if 1<= d <= 30:
+        elif m == 1 or m == 3 or m == 5 or m == 7 or m == 8 or m == 10 m == 12:
+            if 1<= d <= 31:
                 print('Yes')
             else:
                 print('No')
         else:
-            if 1<= d <= 31:
+            if 1<= d <= 30:
                 print('Yes')
             else:
                 print('No')
