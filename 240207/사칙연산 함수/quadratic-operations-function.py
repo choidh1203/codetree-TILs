8c7cf1f6f7937c52398d49ce5a2210a662a89bbb
choidh1203 <98026205+choidh1203@users.noreply.github.com>
2024-02-07 19:31:22 +0900

    Title: 사칙연산 함수, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240207/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,21 @@
+def add(a,b):
+    print(f"{a} + {b} = {a+b}")
+def minus(a,b):
+    print(f"{a} - {b} = {a-b}")
+def times(a,b):
+    print(f"{a} * {b} = {a*b}")
+def divide(a,b):
+    print(f"{a} / {b} = {a/b}")
+def main(a,b):
+    if '+' == o:
+        add(a,b)
+    elif '-' == o:
+        minus(a,b)
+    elif '*' == o:
+        times(a,b)
+    elif '/' == o:
+        divide(a,b)
+
+a,o,c = map(str,input().split())
+a = int(a) ; c = int(c)
+main(a,c)
