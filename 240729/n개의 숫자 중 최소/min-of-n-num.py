1c160ce9e515d5ca15a221bb5500ca0fe0d667d7
choidh1203 <98026205+choidh1203@users.noreply.github.com>
2024-07-29 21:31:04 +0900

    Title: n개의 숫자 중 최소, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240729/n개의 숫자 중 최소/min-of-n-num.py
+++ 240729/n개의 숫자 중 최소/min-of-n-num.py
@@ -1,4 +1,15 @@
+import sys
+
 n = int(input())
-arr = list(map(int,input().split()))
-arr.sort()
-print(arr[0], arr.count(arr[0]))
+arr = list(map(int, input().split()))
+
+min_val = sys.maxsize
+
+idx = 0
+
+for i in range(n):
+    if min_val > arr[i]:
+        min_val = arr[i]
+        idx = i
+
+print(min_val, idx)
