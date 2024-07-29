36fd5d478b535f793f75398fd639bed9c9407d54
choidh1203 <98026205+choidh1203@users.noreply.github.com>
2024-07-30 00:30:09 +0900

    Title: 학점 계산기, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240730/학점 계산기/credit-calculator.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+arr = list(map(float, input().split()))
+sum = 0
+
+for i in range(n):
+    sum += arr[i]
+print("{:.1f}".format(sum/n))
+if sum/n >= 4.0:
+    print('Perfect')
+elif sum/n >= 3.0:
+    print('Good')
+else:
+    print('Poor')
