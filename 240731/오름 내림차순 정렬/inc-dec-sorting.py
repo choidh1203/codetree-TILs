016dd9236a01dbe42c21df34775cc4cdabea61d3
choidh1203 <98026205+choidh1203@users.noreply.github.com>
2024-07-31 19:23:40 +0900

    Title: 오름 내림차순 정렬, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240731/오름 내림차순 정렬/inc-dec-sorting.py
+++ 240731/오름 내림차순 정렬/inc-dec-sorting.py
@@ -1,9 +1,11 @@
 n = int(input())
 numbers = list(map(int,input().split()))
 
+#리스트 정렬
 asc_numbers = sorted(numbers)
 desc_numbers = asc_numbers[::-1]
 
+#정렬된 리스트 출력
 for num in asc_numbers:
     print(num, end=" ")
 print()
