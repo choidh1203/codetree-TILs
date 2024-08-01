1b819b76caeca95c073b44dc30db6338081e81f2
choidh1203 <98026205+choidh1203@users.noreply.github.com>
2024-08-01 15:08:47 +0900

    Title: 줄 세우기 2, Time: 151ms, Memory: 25MB, Status: Passed - CodeTree

--- 240801/줄 세우기 2/line-up-students-2.py
+++ 240801/줄 세우기 2/line-up-students-2.py
@@ -10,7 +10,7 @@ for i in range(1,n+1):
     h, w = map(int,input().split())
     students.append(student(h,w,i))
 
-students.sort(key = lambda x: (x.h,x.w))
+students.sort(key = lambda x: (x.h,-x.w))
 
 for student in students:
     print(f"{student.h} {student.w} {student.idx}")
