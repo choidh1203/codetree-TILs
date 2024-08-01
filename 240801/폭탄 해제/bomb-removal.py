15d0748e233bce4dca2d8e3d0a9a051303a24039
choidh1203 <98026205+choidh1203@users.noreply.github.com>
2024-08-01 13:59:44 +0900

    Title: 폭탄 해제, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240801/폭탄 해제/bomb-removal.py
@@ -0,0 +1,14 @@
+class bomb:
+    def __init__(self, code, color, sec):
+        self.code = code
+        self.color = color
+        self.sec = sec
+
+code, color, sec = input().split()
+
+bomb1 = bomb(code, color, sec)
+
+print(f""""code : {bomb1.code}
+color : {bomb1.color}
+second : {bomb1.sec}
+""")
