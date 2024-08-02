966e9a85f7c03e89c932d0b4629e48e064007121
choidh1203 <98026205+choidh1203@users.noreply.github.com>
2024-08-02 20:45:42 +0900

    Title: DateTime to DateTime, Time: 5014ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

---
+++ 240802/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,24 @@
+a, b, c = tuple(map(int,input().split()))
+
+d = 11
+h = 11
+m = 11
+
+ans = 0
+
+while True:
+    if d == a and h == b and m == c:
+        break
+
+    m+= 1
+    ans += 1
+
+    if m == 60:
+        h += 1
+        m = 0
+    
+    if h == 24:
+        d += 1
+        h = 0
+    
+print(ans)
