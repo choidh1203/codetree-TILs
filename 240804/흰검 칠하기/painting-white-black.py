3c0449cb3276dbbb360ef000c5e146ab1af3e89d
choidh1203 <98026205+choidh1203@users.noreply.github.com>
2024-08-04 13:55:29 +0900

    Title: 흰검 칠하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240804/흰검 칠하기/painting-white-black.py
@@ -0,0 +1,26 @@
+n = int(input())
+
+arr = [[0] for _ in range(11)]
+
+loc = 5
+
+for _ in range(n):
+    x, d = tuple(input().split())
+    x = int(x)
+
+    if d == 'R':
+        for i in range(loc, loc+x):
+            arr[i].append(2)
+        loc = loc+x-1
+    else:
+        for i in range(loc,loc-x,-1):
+            arr[i].append(1)
+        loc = loc-x+1
+
+for i in range(11):
+    if arr[i].count(1) >= 2 and arr[i].count(2) >= 2:
+        arr[i] = 3
+    else:
+        arr[i] = arr[i][-1]
+
+print(arr.count(1),arr.count(2),arr.count(3))
