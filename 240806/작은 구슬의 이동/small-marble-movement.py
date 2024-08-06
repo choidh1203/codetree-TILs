a4ce64e4e889aa9d06525c595a022b4555a2d087
choidh1203 <98026205+choidh1203@users.noreply.github.com>
2024-08-06 21:30:20 +0900

    Title: 작은 구슬의 이동, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240806/작은 구슬의 이동/small-marble-movement.py
+++ 240806/작은 구슬의 이동/small-marble-movement.py
@@ -7,18 +7,20 @@ arr = [[0]*n for _ in range(n)]
 def in_range(x,y,n):
     return 0 <= x and x < n and 0 <= y and y < n
 
-x,y, d = input().split()
-x, y = int(x), int(y)
+y,x, d = input().split()
+y, x = int(y)-1, int(x)-1
 
 s = 0
 
 move = di[d]
 
 while s < t:
-    x, y = x + dx[move], y + dy[move]
-    s += 1
-    if not in_range(x,y,n):
+    nx, ny = x + dx[move], y + dy[move]
+    if in_range(nx, ny, n):
+        x, y = x + dx[move], y + dy[move]
+        s += 1
+    else:
         move = 3 - move
         s += 1
 
-print(x+1,y+1)
+print(y,x)
