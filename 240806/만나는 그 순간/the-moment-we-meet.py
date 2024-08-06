cdac364033363a481b817d7cb5eff2e4bf8f27dd
choidh1203 <98026205+choidh1203@users.noreply.github.com>
2024-08-06 19:31:10 +0900

    Title: 만나는 그 순간, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240806/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,36 @@
+n, m = map(int,input().split())
+
+log_a = [0]
+log_b = [0]
+
+ax = 0
+bx = 0
+
+for i in range(n):
+    d, t = input().split()
+    t = int(t)
+    
+    for j in range(t):
+        if d == 'R':
+            ax += 1
+        else:
+            ax -= 1
+
+        log_a.append(ax)
+
+for i in range(m):
+    d, t = input().split()
+    t = int(t)
+    
+    for j in range(t):
+        if d == 'R':
+            bx += 1
+        else:
+            bx -= 1
+
+        log_b.append(bx)
+
+for k in range(1,len(log_a)):
+    if log_a[k] == log_b[k]:
+        print(k)
+        break
