2af80ceadec3f5a763f4f2826e41c15bcecfea9c
choidh1203 <98026205+choidh1203@users.noreply.github.com>
2024-08-06 21:35:24 +0900

    Title: 작은 구슬의 이동, Time: 148ms, Memory: 24MB, Status: Passed - CodeTree

--- 240806/작은 구슬의 이동/small-marble-movement.py
+++ 240806/작은 구슬의 이동/small-marble-movement.py
@@ -2,8 +2,6 @@ di = {'U':2, 'D':1, 'R':0, 'L':3}
 dx, dy = [1, 0, 0, -1], [0, 1, -1, 0]
 n, t = map(int,input().split())
 
-arr = [[0]*n for _ in range(n)]
-
 def in_range(x,y,n):
     return 0 <= x and x < n and 0 <= y and y < n
 
