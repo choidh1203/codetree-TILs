a4779dbc16c95a7354b41f05b4ade5765b1f2d37
choidh1203 <98026205+choidh1203@users.noreply.github.com>
2024-08-06 19:19:39 +0900

    Title: 연속되는 수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240806/연속되는 수 2/continuous-number2.py
+++ 240806/연속되는 수 2/continuous-number2.py
@@ -2,9 +2,13 @@ n = int(input())
 arr = [int(input()) for _ in range(n)]
 
 cnt = 0
+log = []
 
 for i in range(n):
     if i == 0 or arr[i-1] == arr[i]:
         cnt += 1
+    if arr[i-1] != arr[i]:
+        log.append(cnt)
+        cnt = 1
 
-print(cnt)
+print(max(log))
