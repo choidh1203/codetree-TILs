ede6fd7cbfe3c4eed4ff7cfdd3a1617f7c670621
choidh1203 <98026205+choidh1203@users.noreply.github.com>
2024-08-07 22:29:46 +0900

    Title: 되돌아오기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240807/되돌아오기 2/come-back-2.py
@@ -0,0 +1,21 @@
+dir_num = 3 
+x, y = 0, 0
+dx, dy = [1, 0, -1, 0], [0, -1, 0, 1]
+t= 0
+
+cmd = input()
+
+for i in range(len(cmd)):
+    if cmd[i] == 'L':
+        dir_num = (dir_num - 1 + 4) % 4
+    elif cmd[i] == 'R':
+        dir_num = (dir_num + 1) % 4
+    else:
+        x, y = x + dx[dir_num], y + dy[dir_num]
+    t += 1
+
+    if x == 0 and y ==0:
+        ans = True
+        break
+
+print(t if ans else -1)
